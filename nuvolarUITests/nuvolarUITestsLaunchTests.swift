10d3e306cffdb0db0f6884c74b5b75a14c5cba72
Uladzislau Komar <vladkomar25@gmail.com>
2024-06-06 10:41:29 +0200

    Initial Commit

---
+++ nuvolarUITests/nuvolarUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  nuvolarUITestsLaunchTests.swift
+//  nuvolarUITests
+//
+//  Created by Developer on 6.06.24.
+//
+
+import XCTest
+
+final class nuvolarUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
